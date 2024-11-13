1489b809821e79fe3e0a6362cc2501f58d963088
Antoine <antoine1.gajan@gmail.com>
2024-11-13 20:18:12 +0100

    Ajout des TDs

---
+++ TD5/src/utils.py
@@ -0,0 +1,50 @@
+from scipy import linalg
+import numpy as np
+
+
+class Event:
+    def __init__(
+        self,
+        n_features=10,
+        coefficients=None,
+        tail_strength=0.1,
+        effective_rank=None,
+        noise_level=10,
+    ):
+        self.n_features = n_features
+        self.effective_rank = effective_rank
+
+        if coefficients is None:
+            self.coefficients = 10 * np.random.randn(n_features)
+        else:
+            self.coefficients = coefficients
+
+        coeffs_norm = linalg.norm(self.coefficients)
+        self.model_noise = noise_level / 100 * coeffs_norm
+
+        v, _ = linalg.qr(np.random.randn(n_features, n_features), mode="economic")
+        self._v = v
+
+        # Index of the singular values
+        singular_ind = np.arange(n_features, dtype=np.float64)
+
+        if self.effective_rank is None:
+            tail_strength = 1
+            self.effective_rank = n_features
+            singular_ind = 10 * singular_ind
+
+        # Build the singular profile by assembling signal and noise components
+        low_rank = (1 - tail_strength) * np.exp(
+            -1.0 * (singular_ind / self.effective_rank) ** 2
+        )
+        tail = tail_strength * np.exp(-0.1 * singular_ind / self.effective_rank)
+        self._s = np.identity(n_features) * (low_rank + tail)
+
+    def sample(self, n_samples=100):
+        u0 = np.random.randn(max(n_samples, self.n_features), self.n_features)
+        u, _ = linalg.qr(u0, mode="economic")
+        X = np.dot(np.dot(u, self._s), self._v.T)
+        X = X[:n_samples, :]
+        y = np.dot(X, self.coefficients) + self.model_noise * np.random.randn(n_samples)
+
+        return X, y
